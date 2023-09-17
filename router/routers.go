68dbc672b47e743093a75554dd2eb7519a748de0
Thalison Silva <contato.thalison@outlook.com>
2023-09-17 15:52:24 -0300

    Rotas-creat

--- router/routers.go
+++ router/routers.go
@@ -6,32 +6,35 @@ import (
 )
 
 func InicializaRoutes(router *gin.Engine) {
-	v1 := router.Group("/api/v1") // Correção 1: Usar := para declarar v1 sem usar *gin.RouterGroup
-	{	
-		
-		v1.GET("/opening", func(ctx *gin.Context) { // Correção 2: Colocar a função dentro do GET
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening",
+			})
+		})
+
+		v1.POST("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "POST Opening",
 			})
 		})
-		v1.POST("/opening", func(ctx *gin.Context) { // Correção 2: Colocar a função dentro do GET
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "POST Opening",
-			})
-		})	
-		v1.DELETE("/opening", func(ctx *gin.Context) { // Correção 2: Colocar a função dentro do GET
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "DELETE Opening",
+
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening",
 			})
 		})
-		v1.PUT("/opening", func(ctx *gin.Context) { // Correção 2: Colocar a função dentro do GET
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "PUT Opening",
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT Opening",
 			})
 		})
-		v1.GET("/opening", func(ctx *gin.Context) { // Correção 2: Colocar a função dentro do GET
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "GET Openings",
+
+		v1.GET("/openings", func(ctx *gin.Context) { // Modificado para "/openings"
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Openings",
 			})
 		})
 	}
