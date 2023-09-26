9f8e713ec3d9128856007013a521a0736f04c94c
Thalison Silva <contato.thalison@outlook.com>
2023-09-26 20:55:53 -0300

    Troca-de-teste

--- main.go
+++ main.go
@@ -11,10 +11,3 @@ func main() {
 	// Inicialize Router
 	router.Inicializa()
 }
-func setupRouter(*gin.Engine){
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-	return r
-}
