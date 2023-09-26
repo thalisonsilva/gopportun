f441365888457151f07e5037a8b49587a849ede9
Thalison Silva <contato.thalison@outlook.com>
2023-09-26 20:41:42 -0300

    Add-pacts

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/thalisonsilva/gopportun.git/router"
+	
 )
 
 func main() {
 	// Inicialize Router
 
 	router.Inicializa()
+	fmt.Println("Teste de todas")
 }
